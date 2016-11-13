e7a05542eec73c13a2e9179c3a26e62f0669913a
Sandeep Kumar Pani <sandeepkumarpani888@gmail.com>
2016-11-13 14:33:09 +0530

    Added class.

--- IP_Project.cpp
+++ IP_Project.cpp
@@ -9,6 +9,53 @@ Mat input_img, image, inpaintMask;
 Point prevPt(-1,-1);
 int thickness = 5;
 
+class Inpainter
+{
+public:
+    const static int DEFAULT_HALF_PATCH_WIDTH=3;
+    const static int MODE_ADDITION=0;
+    const static int MODE_MULTIPLICATION=1;
+    const static int ERROR_INPUT_MAT_INVALID_TYPE=0;
+    const static int ERROR_INPUT_MASK_INVALID_TYPE=1;
+    const static int ERROR_MASK_INPUT_SIZE_MISMATCH=2;
+    const static int ERROR_HALF_PATCH_WIDTH_ZERO=3;
+    const static int CHECK_VALID=4;
+
+    Inpainter(cv::Mat inputImage,cv::Mat mask,int halfPatchWidth=4,int mode=1);
+
+    cv::Mat inputImage;
+    cv::Mat mask,updatedMask;
+    cv::Mat result;
+    cv::Mat workImage;
+    cv::Mat sourceRegion;
+    cv::Mat targetRegion;
+    cv::Mat originalSourceRegion;
+    cv::Mat gradientX;
+    cv::Mat gradientY;
+    cv::Mat confidence;
+    cv::Mat data;
+    cv::Mat LAPLACIAN_KERNEL,NORMAL_KERNELX,NORMAL_KERNELY;
+    cv::Point2i bestMatchUpperLeft,bestMatchLowerRight;
+    std::vector<cv::Point> fillFront;
+    std::vector<cv::Point2f> normals;
+    int mode;
+    int halfPatchWidth;
+    int targetIndex;
+
+    int checkValidInputs();
+    void calculateGradients();
+    void initializeMats();
+    void computeFillFront();
+    void computeConfidence();
+    void computeData();
+    void computeTarget();
+    void computeBestPatch();
+    void updateMats();
+    bool checkEnd();
+    void getPatch(cv::Point2i &centerPixel, cv::Point2i &upperLeft, cv::Point2i &lowerRight);
+    void inpaint();
+};
+
 static void onMouse(int event, int x, int y, int flags, void*){
 	if(event == EVENT_LBUTTONUP || flags != EVENT_FLAG_LBUTTON){
 		prevPt = Point(-1,-1);
