6cbdc036e2dce3eb9a1414356f81efb55ba533b4
sayan1994 <sayanrocks11@gmail.com>
2016-11-13 14:37:50 +0530

    removing namespaces

--- IP_Project.cpp
+++ IP_Project.cpp
@@ -21,23 +21,23 @@ public:
     const static int ERROR_HALF_PATCH_WIDTH_ZERO=3;
     const static int CHECK_VALID=4;
 
-    Inpainter(cv::Mat inputImage,cv::Mat mask,int halfPatchWidth=4,int mode=1);
+    Inpainter(Mat inputImage,Mat mask,int halfPatchWidth=4,int mode=1);
 
-    cv::Mat inputImage;
-    cv::Mat mask,updatedMask;
-    cv::Mat result;
-    cv::Mat workImage;
-    cv::Mat sourceRegion;
-    cv::Mat targetRegion;
-    cv::Mat originalSourceRegion;
-    cv::Mat gradientX;
-    cv::Mat gradientY;
-    cv::Mat confidence;
-    cv::Mat data;
-    cv::Mat LAPLACIAN_KERNEL,NORMAL_KERNELX,NORMAL_KERNELY;
-    cv::Point2i bestMatchUpperLeft,bestMatchLowerRight;
-    std::vector<cv::Point> fillFront;
-    std::vector<cv::Point2f> normals;
+    Mat inputImage;
+    Mat mask,updatedMask;
+    Mat result;
+    Mat workImage;
+    Mat sourceRegion;
+    Mat targetRegion;
+    Mat originalSourceRegion;
+    Mat gradientX;
+    Mat gradientY;
+    Mat confidence;
+    Mat data;
+    Mat LAPLACIAN_KERNEL,NORMAL_KERNELX,NORMAL_KERNELY;
+    Point2i bestMatchUpperLeft,bestMatchLowerRight;
+    vector<Point> fillFront;
+    vector<Point2f> normals;
     int mode;
     int halfPatchWidth;
     int targetIndex;
@@ -52,7 +52,7 @@ public:
     void computeBestPatch();
     void updateMats();
     bool checkEnd();
-    void getPatch(cv::Point2i &centerPixel, cv::Point2i &upperLeft, cv::Point2i &lowerRight);
+    void getPatch(Point2i &centerPixel, Point2i &upperLeft, Point2i &lowerRight);
     void inpaint();
 };
 
