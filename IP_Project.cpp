85f2d1512b174910dc60749d57812995e3cdb6d0
Sandeep Kumar Pani <sandeepkumarpani888@gmail.com>
2016-11-13 14:42:56 +0530

    Completed inpaint.

--- IP_Project.cpp
+++ IP_Project.cpp
@@ -71,7 +71,35 @@ public:
     void updateMats();
     bool checkEnd();
     void getPatch(Point2i &centerPixel, Point2i &upperLeft, Point2i &lowerRight);
-    void inpaint();
+    
+    void inpaint(){
+    	namedWindow("updatedMask");
+    	namedWindow("inpaint");
+    	namedWindow("gradientX");
+    	namedWindow("gradientY");
+    	initializeMats();
+    	calculateGradients();
+    	bool notFilled=true;
+    	while(notFilled){
+    		computeFillFront();
+    		computeConfidence();
+    		computeData();
+    		computeTarget();
+    		computeBestPatch();
+    		updateMats();
+    		notFilled=checkEnd();
+
+    		imshow("updatedMask",updatedMask);
+    		imshow("inpaint",workImage);
+    		imshow("gradientX",gradientX);
+    		imshow("gradientY",gradientY);
+    		waitKey(2);
+    	}
+    	result=workImage.clone();
+
+    	namedWindow("confidence");
+    	imshow("confidence",confidence);
+    }
 };
 
 static void onMouse(int event, int x, int y, int flags, void*){
